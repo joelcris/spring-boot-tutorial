070490a44abd798c8f53d94957f1bcfe98727615
joelcris <joel.crisanto.juarez@gmail.com>
2023-02-18 02:52:35 -0600

    added basic components(Service, ServiceImpl, Controller, Repository) for CRUD

---
+++ src/main/java/com/dailycodebuffer/springboot/tutorial/service/DepartmentService.java
@@ -0,0 +1,7 @@
+package com.dailycodebuffer.springboot.tutorial.service;
+
+import com.dailycodebuffer.springboot.tutorial.entity.Department;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+}
