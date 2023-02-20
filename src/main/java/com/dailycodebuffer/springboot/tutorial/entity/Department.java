1791368fe80e92985f121033fe84268aeb09bdb7
joelcris <joel.crisanto.juarez@gmail.com>
2023-02-20 00:00:43 -0600

    added departmentName validation in Department and POST endpoint

--- src/main/java/com/dailycodebuffer/springboot/tutorial/entity/Department.java
+++ src/main/java/com/dailycodebuffer/springboot/tutorial/entity/Department.java
@@ -1,16 +1,20 @@
 package com.dailycodebuffer.springboot.tutorial.entity;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
+    @NotBlank(message = "Please add Department name")
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
