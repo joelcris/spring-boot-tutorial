31177101aa1e9a163d0fac55c34a4507b8cb6034
Joel <30510120+joelcris@users.noreply.github.com>
2023-02-19 23:46:27 -0600

    added GET endpoint for fetch department by name (#7)
    added DELETE endpoint for delete departmet by Id
    
    added PUT endpoint for update department by Id
    
    added GET endpoint for fetch department by name

--- src/main/java/com/dailycodebuffer/springboot/tutorial/repository/DepartmentRepository.java
+++ src/main/java/com/dailycodebuffer/springboot/tutorial/repository/DepartmentRepository.java
@@ -2,8 +2,13 @@ package com.dailycodebuffer.springboot.tutorial.repository;
 
 import com.dailycodebuffer.springboot.tutorial.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    public Department findByDepartmentName(String departmentName);
+
+    public Department findByDepartmentNameIgnoreCase(String departmentName);
 }
