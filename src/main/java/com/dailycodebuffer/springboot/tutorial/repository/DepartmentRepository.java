10b3b844f717adfdf2aa1bd7db5db7b4d34632e0
joelcris <joel.crisanto.juarez@gmail.com>
2023-02-27 00:59:14 -0600

    added comments for DepartmentRepository interface

--- src/main/java/com/dailycodebuffer/springboot/tutorial/repository/DepartmentRepository.java
+++ src/main/java/com/dailycodebuffer/springboot/tutorial/repository/DepartmentRepository.java
@@ -5,10 +5,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for managing Department entities.
+ */
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
+    /**
+     * Finds a department by its name.
+     *
+     * @param departmentName the name of the department to find
+     * @return the Department with the given name, or null if not found
+     */
     public Department findByDepartmentName(String departmentName);
 
+    /**
+     * Finds a department by its name, ignoring case.
+     *
+     * @param departmentName the name of the department to find
+     * @return the Department with the given name, or null if not found
+     */
     public Department findByDepartmentNameIgnoreCase(String departmentName);
 }
